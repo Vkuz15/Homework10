92ecd09ab1bbe3b8d5babda9026f6f551caea47e
Владимир <vkuznecov1504@mail.ru>
2024-01-31 13:31:27 +0400

    С помощью метода toUpperCase преобразовали все знаки в консоли в верхний регистр

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Задача 1
+        //Задача 2
 
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
+        fullName = fullName.toUpperCase();
         System.out.println("Ф.И.О. сотрудника - " + fullName);
     }
 }
